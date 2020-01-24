4593a051c912112881e2c83fffafb3421cb1b728
jillichrome <jmfronk.17@gmail.com>
2020-01-24 15:24:08 -0500

    Add content to 2020

--- src/components/years/2020.js
+++ src/components/years/2020.js
@@ -4,10 +4,88 @@ import './2020.css';
 export default class Year20 extends React.Component {
   render() {
     return(
-      <div className='header'>
-        <h1>2020</h1>
-        <p>GOAL: Baking!</p>
-        <p>Each month a new and complex baked good will be made!</p>
+      <div className='year'>
+        <div className='header'>
+        <span><p>Resolutions are easy to make.</p></span>
+        <span><img src={require('./tree.jpg')} alt='' /></span>
+        <span><p>Difficult to keep.</p></span>
+        </div>
+        <div className='content'>
+          <h1>2020</h1>
+          <p>GOAL: Baking!</p>
+          <div className='card'>
+            <div className='container'>
+              <h2>January</h2>
+              <p>Savory Tart</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>Feburary</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>March</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>April</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>May</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>June</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>July</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>August</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>September</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>October</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>November</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='container'>
+              <h2>December</h2>
+              <p>TBD</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
